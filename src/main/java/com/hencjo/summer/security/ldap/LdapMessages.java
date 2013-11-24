a28f37f909f1b8a2a680088ad23822acfda0968f
Henrik Johansson <hencjo@gmail.com>
2013-11-24 20:36:27 +0100

    More DOC around the messages.

--- src/main/java/com/hencjo/summer/security/ldap/LdapMessages.java
+++ src/main/java/com/hencjo/summer/security/ldap/LdapMessages.java
@@ -3,6 +3,18 @@ package com.hencjo.summer.security.ldap;
 import static com.hencjo.summer.security.ldap.asn.ASN.*;
 
 public class LdapMessages {
+/*
+ * BindResponse ::= [APPLICATION 1] SEQUENCE {
+ *           COMPONENTS OF LDAPResult}
+ * 
+ * LDAPResult ::= SEQUENCE {
+ *           resultCode         ENUMERATED {
+ *                success                      (0),
+ *                ...},
+ *           matchedDN          LDAPDN,
+ *           diagnosticMessage  LDAPString,
+ *           ... }   
+ */
 	public static final class BindResponse {
 		public final AsnInteger messageId = integer();
 		public final AsnEnumerated resultCode = enumerated();
@@ -11,6 +23,16 @@ public class LdapMessages {
 		public final AsnSequence message = sequence(messageId, application(1, resultCode, matchedDn, diagnosticMessage));
 	}
 
+/*
+ *   BindRequest ::= [APPLICATION 0] SEQUENCE {
+ *           version                 INTEGER (1 ..  127),
+ *           name                    LDAPDN,
+ *           authentication          AuthenticationChoice }
+ *
+ *      AuthenticationChoice ::= CHOICE {
+ *           simple                  [0] OCTET STRING,
+ *           ...  }
+ */
 	public static final class BindRequest {
 		public final AsnInteger messageId = integer();
 		public final AsnInteger protocolVersion = integer();
@@ -19,7 +41,9 @@ public class LdapMessages {
 		public final AsnSequence message = sequence(messageId, application(0, protocolVersion, username, choice(0, password)));
 	}
 	
-//	UnbindRequest ::= [APPLICATION 2] NULL
+/*
+ * 	UnbindRequest ::= [APPLICATION 2] NULL
+ */
 	public static final class UnbindRequest {
 		public final AsnInteger messageId = integer();
 		public final AsnString nullValue = nullValue();
